0d9f3ca06450a35c79208da81b95025d91f04984
Felipe Toscano <felipe.toscano@gmail.com>
2017-09-15 00:42:39 -0300

    teste vsc

--- web/index.php
+++ web/index.php
@@ -2,32 +2,6 @@
 require('../vendor/autoload.php');
 require('bootstrap.php');
 
-<<<<<<< HEAD
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function () use ($app) {
-    $app['monolog']->addDebug('logging output.');
-    return $app['twig']->render('index.twig');
-})->bind('index');
-
-$app->mount('/viagem', new Trwip\Controller\ViagemControllerProvider());
-
-$app->get('/contato', function () use ($app) {
-    $app['monolog']->addDebug('enter in contact page.');
-=======
 //Rotas do site
 $app->get('/', function () use ($app) {
     $app['monolog']->addDebug('Chamou a rota /');
@@ -36,7 +10,6 @@ $app->get('/', function () use ($app) {
 
 $app->get('/contato', function () use ($app) {
     $app['monolog']->addDebug('Chamou a rota /contato');
->>>>>>> c4ec3b73c302ee0eacc1743bb0d745b52bdc589c
     return $app['twig']->render('contact.twig');
 })->bind('contact');
 
